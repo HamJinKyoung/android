7e740517f8515c7d800a71179ef47f0a9f1f6c27
HamJinKyoung <wlsrud3803@gmail.com>
2018-11-24 23:32:28 +0900

    ListView Update

---
+++ ListViewUpdateTest/app/src/main/java/cse/mobile/listviewupdatetest/MainActivity.java
@@ -0,0 +1,24 @@
+package cse.mobile.listviewupdatetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> al = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>;
+
+
+    }
+}
